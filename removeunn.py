328fc10904845a2b3246fa8ffff4fe59d4df7f56
Abraham Asfaw <abraham.asfaw@acm.org>
2012-04-19 15:16:41 -0400

    Added removeun.py and modified compile

---
+++ removeunn.py
@@ -0,0 +1,25 @@
+import sys
+files = sys.argv[1:]
+
+for afile in files:
+    lines = open(afile).read().split("\n")
+    startindices = []
+    endindices = []
+    for i in xrange(len(lines)):
+        if "\\beq" in lines[i] or "\\begin{align}" in lines[i]:
+            triggeron = True
+            startindices.append(i)
+        elif "\\eeq" in lines[i] or "\\end{align}" in lines[i] or "\\end{align*}" in lines[i]:
+            triggeron = False
+            endindices.append(i)
+
+    for start,end in zip(startindices,endindices):
+        if "\\label" not in " ".join(lines[start:end]):
+            if "\\beq" in lines[start]:
+                lines[start] = lines[start].replace("\\beq","\\beqnn")
+                lines[end] = lines[end].replace("\\eeq","\\eeqnn")
+            elif "\\begin{align}" in lines[start]:
+                lines[start] = lines[start].replace("\\begin{align}","\\begin{align*}")
+                lines[end] = lines[end].replace("\\end{align}","\\end{align*}")
+
+    open(afile,'w').write("\n".join(lines))
